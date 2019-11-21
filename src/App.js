a8457755c5c5da1ff799bc9e737337ce1d604a2f
cynthiaChanChan <mession01@gmail.com>
2019-11-21 12:48:42 +0800

    Create a sass folder to hold all style files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './App.scss';
+import './sass/app.scss';
 import HomePage from './pages/homepage/homepage';
 
 const App = () => {
--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import './menu-item.scss';
 
 class MenuItem extends React.Component {
 
